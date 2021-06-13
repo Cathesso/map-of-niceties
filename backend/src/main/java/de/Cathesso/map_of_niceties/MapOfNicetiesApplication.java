7f1de6f0d9714e3521ea47d659e907ec18d0d38a
Cathesso <h-f.fromme@posteo.de>
2021-06-13 21:48:17 +0200

    Init Project

---
+++ backend/src/main/java/de/Cathesso/map_of_niceties/MapOfNicetiesApplication.java
@@ -0,0 +1,13 @@
+package de.Cathesso.map_of_niceties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapOfNicetiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapOfNicetiesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/Cathesso/map_of_niceties/MapOfNicetiesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.Cathesso.map_of_niceties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapOfNicetiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
